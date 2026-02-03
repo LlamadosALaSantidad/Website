a2e14e38d5d9d00f8900772519217a7c1d9baabb
Izzyverse <alfaomega.orso@parque-e.co>
2026-02-02 23:28:13 -0500

    v1.26

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { HelmetProvider } from 'react-helmet-async'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      <SpeedInsights/>
     </HelmetProvider>
   </StrictMode>,
 )
