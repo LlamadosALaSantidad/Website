4fa17c64bd744c4ea08fcfc3a740a1c57231abc3
Izzyverse <alfaomega.orso@parque-e.co>
2026-02-03 13:30:03 -0500

    v1.3

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -8,7 +8,7 @@ function Hero() {
     return (
         <section className="hero">
             <span className="badge">Bienvenidos peregrinos</span>
-            <h1>En vías de <span>Santidad</span></h1>
+            <h1>Llamados a la <span>Santidad</span></h1>
             <p>{SITE_INFO.description}</p>
             <div className="buttons">
                 <Link to={PATHS.SAINTS}><Button variant="primary">Explorar Santos</Button></Link>
